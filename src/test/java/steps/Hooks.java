1d084044f51db84814d2fd7e51f6e5c6a3c25dd4
Andras Fuge <andrisregek@gmail.com>
2018-05-13 18:34:59 +0200

    add scrrenshot taking to Hooks

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -4,6 +4,7 @@ import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import driver.DriverManager;
+import driver.DriverMethods;
 import pageobjects.OrderPage;
 
 public class Hooks {
@@ -14,7 +15,11 @@ public class Hooks {
 
     @After
     public void afterScenario(Scenario scenario) {
-        if (scenario.isFailed()) {
+        if (scenario.isFailed()) {try {
+            scenario.embed(DriverMethods.takeScreenShot(),"image/png");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         }
         OrderPage orderPage = new OrderPage();
         if (orderPage.isLogoutAvailable()) {
